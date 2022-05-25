bd1af6ea5476805bfdd93837bf7a6e7d99903892
edsonwade <vanilsonmuhongo@gmail.com>
2022-05-25 15:17:47 +0100

    Project intro

---
+++ src/main/java/mindswap/academy/urlcompress/UrlCompressApplication.java
@@ -0,0 +1,14 @@
+package mindswap.academy.urlcompress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCompressApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UrlCompressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mindswap/academy/urlcompress/UrlCompressApplicationTests.java
@@ -0,0 +1,13 @@
+package mindswap.academy.urlcompress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCompressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
