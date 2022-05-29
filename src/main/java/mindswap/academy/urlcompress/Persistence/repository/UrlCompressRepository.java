556f7fa1f2c49a7e0ef2318890e2d749015afd56
vanil <vanil@DESKTOP-EPO41LJ>
2022-05-29 22:57:25 +0100

    [Task004]-[Controller Implementation]

--- src/main/java/mindswap/academy/urlcompress/Persistence/repository/UrlCompressRepository.java
+++ src/main/java/mindswap/academy/urlcompress/Persistence/repository/UrlCompressRepository.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UrlCompressRepository extends JpaRepository<UrlCompress,Integer> {
 
-    @Query(value="select full_url " +
+    @Query(value="select long_url " +
             "from" +
-            " Url where short_url = ?1",
+            " Url where short_url =?1",
             nativeQuery = true)
 
     String findByShortUrl(String id);
---
+++ src/main/java/mindswap/academy/urlcompress/controller/UrlCompressController.java
@@ -0,0 +1,42 @@
+package mindswap.academy.urlcompress.controller;
+
+import lombok.RequiredArgsConstructor;
+import mindswap.academy.urlcompress.Persistence.model.UrlCompress;
+import mindswap.academy.urlcompress.service.UrlCompressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/api/v1/")
+@RequiredArgsConstructor
+public class UrlCompressController {
+
+    private final UrlCompressService urlCompressService;
+
+    @GetMapping(value="hello")
+   public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok().body("Hello world");
+   }
+
+    @GetMapping(value = "/url")
+    public ResponseEntity<List<UrlCompress>> findAllUrl() {
+        return ResponseEntity.ok().body(urlCompressService.getAllUrl());
+
+    }
+
+    @GetMapping(value="/url/{id}")
+    public ResponseEntity<?>getOriginalUrl(@PathVariable(name="id") String id){
+          return ResponseEntity.ok().body(urlCompressService.getFullUrl(id));
+    }
+
+    @PostMapping(value="/url/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UrlCompress generateShortUrl(@Valid @RequestBody String url){
+        return urlCompressService.createShortUrl(url);
+    }
+}
