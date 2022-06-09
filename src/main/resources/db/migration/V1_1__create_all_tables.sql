0fd00ecf0bd8e9ccc4f31246c888142a23ef1beb
Duarte Figueiredo <duarte.figueiredo@outlook.com>
2022-06-09 15:23:47 +0100

    Fix database exists bug

--- src/main/resources/db/migration/V1_1__create_all_tables.sql
+++ src/main/resources/db/migration/V1_1__create_all_tables.sql
@@ -1,11 +1,13 @@
-create database urlcompress;
 drop table if exists url;
 
-create table url(id integer not null auto_increment,
-                     long_url varchar(255) not null,
-                     short_url varchar(255) not null,
-                     primary key (id)
-)engine=InnoDB  default charset=utf8;
+create table url
+(
+    id        integer      not null auto_increment,
+    long_url  varchar(255) not null,
+    short_url varchar(255) not null,
+    primary key (id)
+) engine = InnoDB
+  default charset = utf8;
 
 alter table url
-add constraint UK_ru951hscq31qaip8joempxej9 unique (long_url);
+    add constraint UK_ru951hscq31qaip8joempxej9 unique (long_url);
